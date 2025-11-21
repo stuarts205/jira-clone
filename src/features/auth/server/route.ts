c3933c362392adf1b2637a4f79b28cae5b6c392e
DFCU\stuart.smith <stuarts205@gmail.com>
2025-11-21 11:47:02 -0700

    05: authorize user

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -5,8 +5,13 @@ import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
 import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -18,7 +23,7 @@ const app = new Hono()
       httpOnly: true,
       secure: true,
       sameSite: "strict",
-      maxAge: 60 * 60 * 24 * 30, // 30 days
+      maxAge: 60 * 60 * 24 * 30, 
     });
 
     return c.json({ success: true });
@@ -40,8 +45,10 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
     return c.json({ success: true });
   });
 
