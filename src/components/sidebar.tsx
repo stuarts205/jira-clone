ee6666066781784ae3795799b97aec992cc26b0a
DFCU\stuart.smith <stuarts205@gmail.com>
2025-12-01 11:12:42 -0700

    09: workspace  switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -11,6 +12,8 @@ export const Sidebar = () => {
         <Link href='/'>
             <Image src='/logo.svg' alt="Logo" width={50} height={50} />
         </Link>
+        <DottedSeparator className="my-4" />
+        <WorkspaceSwitcher />
         <DottedSeparator className="my-4" />
         <Navigation />
     </aside>
