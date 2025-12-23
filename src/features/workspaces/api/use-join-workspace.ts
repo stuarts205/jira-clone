e52a1a59345ef5846cd2967acea39f037c532020
DFCU\stuart.smith <stuarts205@gmail.com>
2025-12-23 10:10:20 -0700

    17: invite system

---
+++ src/features/workspaces/api/use-join-workspace.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { client } from "@/lib/rpc";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<(typeof client.api.workspaces)[':workspaceId']["join"]["$post"], 200>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[':workspaceId']["join"]["$post"]
+>;
+
+export const useJoinWorkspace = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({param, json}) => {
+      const response = await client.api.workspaces[':workspaceId']["join"]["$post"]({ param, json });
+      if (!response.ok) {
+        throw new Error('Joining workspace failed');
+      }
+      return await response.json();
+    },
+    onSuccess: ({data}) => {
+      toast.success("Joined workspace successfully");
+      queryClient.invalidateQueries({ queryKey: ['workspaces'] })
+      queryClient.invalidateQueries({ queryKey: ['workspace', data.$id] })
+    },
+    onError: (error) => {
+      toast.error(`Error joining workspace`);
+    }
+  });
+
+  return mutation;
+};
